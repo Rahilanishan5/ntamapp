58a2c6dd334a89c0f7bf187921ed08b0fe9b438d
Rahilanishan5 <rahilanishan5@gmail.com>
2024-05-15 10:43:26 +0400

    Initial Commit

---
+++ ntamapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ntamapp
+//
+//  Created by RAHILA NISHAN on 15/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
